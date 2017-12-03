a18f4965c28ebdd251dd360d115089cd19299dd8
urbanjagodic <34213584+urbanjagodic@users.noreply.github.com>
2017-12-03 23:01:01 +0100

    added fade in animations on start up, added image icon files, added xml drawable files, added java classes for animation

--- app/src/main/java/emp/quezy/main/Main.java
+++ app/src/main/java/emp/quezy/main/Main.java
@@ -2,17 +2,101 @@ package emp.quezy.main;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageView;
 
 import emp.quezy.R;
+import emp.quezy.helper.HelperMethods;
+import emp.quezy.other.MyAnimation;
 
 public class Main extends AppCompatActivity {
+
+
+    private ImageView[] startButtons;
+    private String TAG = getClass().getSimpleName().toLowerCase();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        startButtons = new ImageView[]{
+                findViewById(R.id.playButton), findViewById(R.id.settingsButton), findViewById(R.id.infoButton)
+        };
+
+        animateButtons();
+        buttonAction();
+
 
 
     }
+
+
+
+    public void buttonAction() {
+        for (ImageView button : startButtons) {
+
+            button.setOnTouchListener(new View.OnTouchListener() {
+                @Override
+                public boolean onTouch(View view, MotionEvent motionEvent) {
+
+                    if (motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
+                        switch (view.getId()) {
+                            case R.id.playButton:
+                                ((ImageView) view).setImageResource(R.drawable.play_image_pressed);
+                                break;
+                            case R.id.settingsButton:
+                                ((ImageView) view).setImageResource(R.drawable.settings_image_pressed);
+                                break;
+                            case R.id.infoButton:
+                                ((ImageView) view).setImageResource(R.drawable.info_image_pressed);
+                                break;
+                        }
+                    } else if (motionEvent.getAction() == MotionEvent.ACTION_UP) {
+                        switch (view.getId()) {
+                            case R.id.playButton:
+                                ((ImageView) view).setImageResource(R.drawable.play_image);
+                                break;
+                            case R.id.settingsButton:
+                                ((ImageView) view).setImageResource(R.drawable.settings_image);
+                                break;
+                            case R.id.infoButton:
+                                ((ImageView) view).setImageResource(R.drawable.info_image);
+                                break;
+                        }
+                    }
+                    return  false;
+                }
+
+            });
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+
+                    switch (view.getId()) {
+
+                        case R.id.playButton:
+                            HelperMethods.showToast(Main.this, R.id.playButton + "");
+                            break;
+                        case R.id.settingsButton:
+                            HelperMethods.showToast(Main.this, R.id.settingsButton + "");
+                            break;
+                        case R.id.infoButton:
+                            HelperMethods.showToast(Main.this, R.id.infoButton + "");
+                            break;
+                    }
+                }
+            });
+        }
+    }
+
+    public void animateButtons() {
+        for (ImageView button : startButtons) {
+
+            new MyAnimation().fadeIn(button, getApplicationContext(), R.anim.fade_in);
+        }
+
+    }
+
 }
--- app/src/main/java/emp/quezy/other/SplashScreen.java
+++ app/src/main/java/emp/quezy/other/SplashScreen.java
@@ -1,10 +1,11 @@
-package emp.quezy.other.splashscreen;
+package emp.quezy.other;
+
+import android.support.v7.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.os.CountDownTimer;
-import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.ImageView;
 
@@ -28,7 +29,7 @@ public class SplashScreen extends AppCompatActivity {
         };
 
 
-        new CountDownTimer(6000, 400) {
+        new CountDownTimer(7000, 300) {
 
             public void onTick(long millisUntilFinished) {
 
@@ -37,6 +38,9 @@ public class SplashScreen extends AppCompatActivity {
                 }
                 Log.d("tick", millisUntilFinished + "");
                 for (int i = 0; i < circles.length ; i++) {
+                    if (i == index) {
+                        continue;
+                    }
                     circles[i].setColorFilter(Color.WHITE);
                 }
                 circles[index++].setColorFilter(R.color.splash_screen_circle);
